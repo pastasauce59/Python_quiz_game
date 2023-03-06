c8454fea32b10009aa332780040cf4049df5ebf0
Michael Primavera <pastasauce59@gmail.com>
2023-03-05 22:31:06 -0500

    Created method 'still_has_questions' in quiz_brain, created while loop in main.py for as long as the quiz contains questions it will keep asking each one until it runs out.

--- main.py
+++ main.py
@@ -11,4 +11,6 @@ for question in question_data:
 # print(question_bank[0].answer)
 
 quiz = QuizBrain(question_bank)
-quiz.next_question()
+
+while quiz.still_has_questions():
+    quiz.next_question()
